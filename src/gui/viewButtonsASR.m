c313005892523ef0af6b04e368774015074f3902
MCMF <>
2018-05-23 16:02:46 +0200

    bug fix in viewButtonsASR.m

--- src/gui/viewButtonsASR.m
+++ src/gui/viewButtonsASR.m
@@ -10,16 +10,16 @@ FontPropsA.FontUnits = 'normalized';
 FontPropsA.FontSize = 0.8;
 % Rotate-buttons
 uicontrol('Units','normalized','Position',[0.5-BSX*3/2     0.01 BSX BSY],FontPropsA,...
-    'String','Back','Callback','mouseControl3d(gca, [0 0 1 0; -1 0 0 0; 0 1 0 0; 0 0 0 1])');
+    'String','Posterior','Callback','mouseControl3d(gca, [0 0 -1 0; -1 0 0 0; 0 1 0 0; 0 0 0 1])');
 uicontrol('Units','normalized','Position',[0.5-BSX*3/2 0.01+BSY BSX BSY],FontPropsA,...
-    'String','Front','Callback','mouseControl3d(gca, [0 0 1 0; 1 0 0 0; 0 1 0 0; 0 0 0 1])');
+    'String','Anterior','Callback','mouseControl3d(gca, [0 0 1 0; 1 0 0 0; 0 1 0 0; 0 0 0 1])');
 uicontrol('Units','normalized','Position',[0.5-BSX*1/2 0.01+BSY BSX BSY],FontPropsA,...
-    'String','Top','Callback','mouseControl3d(gca, [0 0 1 0;0 1 0 0; -1 0 0 0; 0 0 0 1])');
+    'String','Superior','Callback','mouseControl3d(gca, [0 0 1 0;0 1 0 0; -1 0 0 0; 0 0 0 1])');
 uicontrol('Units','normalized','Position',[0.5-BSX*1/2     0.01 BSX BSY],FontPropsA,...
-    'String','Bottom','Callback','mouseControl3d(gca, [0 0 1 0;0 -1 0 0; 1 0 0 0; 0 0 0 1])');
+    'String','Inferior','Callback','mouseControl3d(gca, [0 0 1 0;0 -1 0 0; 1 0 0 0; 0 0 0 1])');
 uicontrol('Units','normalized','Position',[0.5+BSX*1/2 0.01+BSY BSX BSY],FontPropsA,...
     'String','Left','Callback','mouseControl3d(gca, [1 0 0 0;0 0 -1 0; 0 1 0 0; 0 0 0 1])');
 uicontrol('Units','normalized','Position',[0.5+BSX*1/2     0.01 BSX BSY],FontPropsA,...
-    'String','Right','Callback','mouseControl3d(gca, [1 0 0 0;0 0 1 0; 0 1 0 0; 0 0 0 1])');
+    'String','Right','Callback','mouseControl3d(gca, [-1 0 0 0;0 0 1 0; 0 1 0 0; 0 0 0 1])');
 
 end
