e444eac226dba31162f2db9859ee177c06c5e8b6
MCMF <>
2018-12-17 13:58:49 +0100

    bug fix in lineToVertexIndices.m

--- src/lineToVertexIndices.m
+++ src/lineToVertexIndices.m
@@ -2,11 +2,8 @@ function Idx = lineToVertexIndices(line, mesh)
 
 [linePts, linePos] = intersectLineMesh3d(line, mesh.vertices, mesh.faces);
 [linePts, linePtsIdx] = unique(linePts,'rows','stable');
-linePos=linePos(linePtsIdx);
-if linePos(1)>linePos(end)
-    linePts=flipud(linePts);
-    linePos=flipud(linePos);
-end
+[~, linePosIdx]=sort(linePos(linePtsIdx));
+linePts=linePts(linePosIdx,:);
 [~, Idx] = pdist2(mesh.vertices,[linePts(1,:);linePts(end,:)],'euclidean','Smallest',1);
 
 end
