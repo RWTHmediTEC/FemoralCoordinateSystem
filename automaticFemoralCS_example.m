ad7fe2a96d8be3b3676e67136e03ef3cb9727704
MCMF <>
2020-08-23 21:41:45 +0200

    new examples

--- automaticFemoralCS_example.m
+++ automaticFemoralCS_example.m
@@ -1,13 +1,32 @@
 clearvars; close all; opengl hardware
 
-% Load example data
-load('data\F.mat')
+% Clone example data
+if ~exist('VSD', 'dir')
+    try
+    !git clone https://github.com/RWTHmediTEC/VSDFullBodyBoneModels VSD
+    rmdir('VSD/.git', 's')
+    catch
+        warning([newline 'Clone (or copy) the example data from: ' ...
+            'https://github.com/RWTHmediTEC/VSDFullBodyBoneModels' newline 'to: ' ...
+            fileparts([mfilename('fullpath'), '.m']) '\VSD' ...
+            ' and try again!' newline])
+        return
+    end
+end
 
-% Select example
-Idx = 1;
+% Load subject names
+load('VSD\MATLAB\res\VSD_Subjects.mat', 'Subjects')
+sides = {'R','L'};
 
-[fwTFM2AFCS, LMIdx, HJC, LM] = automaticFemoralCS(F(Idx).mesh, F(Idx).side,...
-    'definition','MediTEC', 'visu',1, 'verb',0, 'debug',1);
+for s=1%:size(Subjects, 1)
+    load(['VSD\Bones\' Subjects.Number{s} '.mat'],'B');
+    
+    sideIdx = randi(2);
+    
+    [fwTFM2AFCS, LMIdx, HJC, LM] = automaticFemoralCS(B(sideIdx+3).mesh, sides{sideIdx},...
+        'definition','MediTEC', 'visu',1, 'verb',0, 'debug',1);
+    
+end
 
 % [List.f, List.p] = matlab.codetools.requiredFilesAndProducts([mfilename '.m']); 
 % List.f = List.f'; List.p = List.p';
